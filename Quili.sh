afb8342e90d20986807488cba23c80d549ca7b06
Vantone <39580709+Vantone@users.noreply.github.com>
2024-10-25 21:20:53 +0800

    Create Quili.sh

---
+++ Quili.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# Qclient文件 linux下载命令：
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.1
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.2
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.3
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.4
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.6
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.8
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.12
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.13
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.15
+wget https://releases.quilibrium.com/qclient-2.0.1-linux-amd64.dgst.sig.16
+chmod +x  qclient-2.0.1-linux-amd64
+
