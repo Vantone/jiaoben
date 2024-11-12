7542ef3f92a022457c8d0b64cbe015b6535ec838
Vantone <39580709+Vantone@users.noreply.github.com>
2024-11-12 22:26:12 +0800

    Update qclient.sh

--- qclient.sh
+++ qclient.sh
@@ -15,3 +15,14 @@ wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.15
 wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.16
 chmod +x  qclient-2.0.3-linux-amd64
 
+# node文件下载命令
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.1
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.3
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.4
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.7
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.13
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.15
+wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64.dgst.sig.16
+chmod +x node-2.0.3.3-linux-amd64
