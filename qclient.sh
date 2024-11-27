e0c0e50658e376349665e8e8752376998816c69c
Vantone <39580709+Vantone@users.noreply.github.com>
2024-11-28 02:11:30 +0800

    Update qclient.sh

--- qclient.sh
+++ qclient.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
 # Qclient文件 linux下载命令：
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.1
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.2
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.4
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.7
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.13
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.15
-wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.16
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.1
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.2
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.4
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.7
+wget https://releases.quilibrium.com//qclient-2.0.4.1-linux-amd64.dgst.sig.13
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.15
+wget https://releases.quilibrium.com/qclient-2.0.4.1-linux-amd64.dgst.sig.16
 chmod +x  qclient-2.0.4.1-linux-amd64
 
 # node文件下载命令
