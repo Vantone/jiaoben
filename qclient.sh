f45e65ebeb0a5847cc5d5cfb3f54970a0968fb2d
Vantone <39580709+Vantone@users.noreply.github.com>
2024-11-28 02:03:01 +0800

    Update qclient.sh

--- qclient.sh
+++ qclient.sh
@@ -1,19 +1,16 @@
 #!/bin/bash 
 
 # Qclient文件 linux下载命令：
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.1
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.2
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.3
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.4
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.6
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.8
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.12
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.13
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.15
-wget https://releases.quilibrium.com/qclient-2.0.3-linux-amd64.dgst.sig.16
-chmod +x  qclient-2.0.3-linux-amd64
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.1
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.2
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.4
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.7
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.13
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.15
+wget https://releases.quilibrium.com/qclient-release/qclient-2.0.4.1-linux-amd64.dgst.sig.16
+chmod +x  qclient-2.0.4.1-linux-amd64
 
 # node文件下载命令
 wget https://releases.quilibrium.com/release/node-2.0.3.3-linux-amd64
